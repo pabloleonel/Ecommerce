478d2dbfbc1cd1094f6df12a97f66f28b017156d
pabloleonel <pabloleonelmello@gmail.com>
2021-08-19 14:49:32 -0300

    dvd

--- js/login.js
+++ js/login.js
@@ -2,9 +2,21 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 function onSignIn(){
-    window.location.href="home.html"
+    window.location.href ="hme.html";
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-
+/*document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("ingresar-bottom").addEventListener("click", 
+    function(event){
+        balidacion();
+    })   
 });
+
+function validacion(){
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    if((user !=="") && (pass !=="") && (user.length<=6 && user.length>=15)){
+       window.location.href="home.html"; 
+    }
+    
+}*/
