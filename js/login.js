4664c49466fad6dc60f949bfc2c51cb573ba333c
pabloleonel <pabloleonelmello@gmail.com>
2021-08-20 11:40:33 -0300

    updates

--- js/login.js
+++ js/login.js
@@ -2,21 +2,5 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 function onSignIn(){
-    window.location.href ="hme.html";
+    window.location.href ="home.html";
 }
-
-/*document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("ingresar-bottom").addEventListener("click", 
-    function(event){
-        balidacion();
-    })   
-});
-
-function validacion(){
-    let user = document.getElementById("user").value;
-    let pass = document.getElementById("pass").value;
-    if((user !=="") && (pass !=="") && (user.length<=6 && user.length>=15)){
-       window.location.href="home.html"; 
-    }
-    
-}*/
