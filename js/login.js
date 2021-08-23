b96f7e99db28a1570efb2bd3d7623c1dc25ccb81
pabloleonel <pabloleonelmello@gmail.com>
2021-08-23 13:03:45 -0300

    upd

--- js/login.js
+++ js/login.js
@@ -2,5 +2,5 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 function onSignIn(){
-    window.location.href ="home.html";
+    window.location.href = "home.html";
 }
