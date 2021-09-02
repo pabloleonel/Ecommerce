a42535576e969a91e4283497cc451b16684bfd8e
pabloleonel <pabloleonelmello@gmail.com>
2021-09-02 19:27:36 -0300

    sss

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-function onSignIn(){
-    window.location.href ="home.html";
+let miStorage = window.localStorage;
+let userName = document.getElementById("user");
+let password = document.getElementById("pass");
+
+function validar() { // funcion de la validacion del login.
+    const home = "home.html"; //redireccion
+    miStorage.setItem('nombreUsuario', userName.value); //guardo el usuario en el localstorage
+    if (userName.value === "" || password.value === "") {
+        alert("Campos incompletos, algún campo vacio.");
+    }
+    else {
+        window.location.href = "home.html"
+    }}
+
+function onSignIn(googleUser) { // poner username en el navbar, pero con google. 
+    var perfil = googleUser.getBasicProfile();
+    miStorage.setItem('nombreUsuario', perfil.getName());
+    window.location.href = "home.html";
 }
+
