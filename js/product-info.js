2e4f8cf84d759d78f540e4ddeb5596fc76c26313
pabloleonel <pabloleonelmello@gmail.com>
2021-09-09 18:02:53 -0300

    f

--- js/product-info.js
+++ js/product-info.js
@@ -8,15 +8,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
 fetch(PRODUCT_INFO_URL)
     .then(respuesta => respuesta.json())
     .then(datos => {
-        let productNameHTML = document.getElementById("productName");
-        let productDescriptionHTML = document.getElementById("productDescription"); 
-        let costProductHTML = document.getElementById("costProduct"); 
-        let soldCountProductHTML = document.getElementById("soldCountProduct"); 
 
-        productNameHTML.innerHTML =  datos.name 
-        productDescriptionHTML.innerHTML = "Descripcion: <br>" + datos.description
-        costProductHTML.innerHTML = "Costo: " + datos.currency + " " + datos.cost
-        soldCountProductHTML.innerHTML = "Cantidad disponible: " + datos.soldCount
+        document.getElementById("productName").innerHTML = datos.name
+        document.getElementById("productDescription").innerHTML = "Descripcion: <br>" + datos.description
+        document.getElementById("costProduct").innerHTML = "Costo: " + datos.currency + " " + datos.cost
+        document.getElementById("soldCountProduct").innerHTML = "Cantidad disponible: " + datos.soldCount
         document.getElementById('showProduct').setAttribute("src", datos.images[0])
         document.getElementById('showProduct1').setAttribute("src", datos.images[1])
         document.getElementById('showProduct2').setAttribute("src", datos.images[2])
@@ -24,4 +20,9 @@ fetch(PRODUCT_INFO_URL)
 
     })
     .catch(error => alert("Hubo un error: " + error));
-
+    
+    fetch(PRODUCT_INFO_COMMENTS_URL)
+        .then(response => response.json())
+        .then(comentario => {
+            document.getElementById("comentarios").setAttribute("span", comentario.description) 
+        })  
