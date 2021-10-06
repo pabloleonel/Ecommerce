29fe691d1b244c8de517a3c03f5cd1414ffabc18
pabloleonel <pabloleonelmello@gmail.com>
2021-10-06 00:46:23 -0300

    updates

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,85 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+let productosCarrito;
 
-});
+function calculateTotal(){ // CALCULAR Y MOSTRAR EL TOTAL
+  
+  let sumaTotal = 0;
+  for(let i = 0; i < productosCarrito.articles.length ;i++){
+    sumaTotal += parseFloat(document.getElementById(`subtotal${i}`).innerHTML); // sumatoria de todos los subtotales q hayan. 
+  }
+
+  document.getElementById('total').innerHTML = sumaTotal; // imprimir ese dato. 
+}
+
+function multiplyPrice(i){ // multiplicar el precio x cant de productos. 
+  let precio = parseFloat(document.getElementById(`precio${i}`).innerText); // trae el valor de precio unitario
+  let cantidad = parseFloat(document.getElementById(`cantidad${i}`).value); // trae el valor de la cantidad de prods 
+
+  let total = precio * cantidad; // multiplica la cantidad de productos por el precio unitario
+
+  document.getElementById(`subtotal${i}`).innerHTML = total; //imprimo el total de todos los productos y sus respectivos precios unitarios x la cantidad de productos que se quiere adquirir.
+  calculateTotal();
+}
+
+function goToProducts(){ // funcionalidad del boton de ir a productos.
+  window.location.href = "products.html"
+}
+
+function showCartProduct(){ // mostrar productos
+  htmlTexto = "";
+  let contador = 0;
+
+  for (let i = 0; i < productosCarrito.articles.length; i++) { // for que recorre la lista de prods. 
+    const producto = productosCarrito.articles[i];
+    
+    htmlTexto += `
+    <li class="cart_item clearfix">
+      <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
+      
+        <div class="cart_item_image cart_info_col">
+          <img src="${producto.src}"/>
+        </div>
+
+        <div class="cart_item_quantity cart_info_col">
+          <div class="cart_item_title">Quantity</div>
+          <input id="cantidad${i}" class="cart_item_text" min="1" value="1" type="number" onchange="multiplyPrice(${i})"/>
+        </div>
+        <div class="cart_item_price cart_info_col">
+          <div class="cart_item_title">Price</div>
+          <div id="precio${i}" class="cart_item_text">${producto.unitCost}</div>
+        </div>
+        <div class="cart_item_subtotal cart_info_col">
+          <div class="cart_item_title">Subtotal</div>
+          <div id="subtotal${i}" class="cart_item_text">${producto.unitCost}</div>
+        </div>
+      </div>
+    </li>
+    `;
+
+    contador++; //SUMARLE 1 PRODUCTO
+  }
+
+  document.getElementById('cartList').innerHTML = htmlTexto //inserta el formato de arriba
+  document.getElementById('cantidadTotal').innerHTML = ` (${contador} productos en el carrito)`; // dice cuantos productos hay en el carrito
+  calculateTotal();
+}
+
+function finishPurchase(){ // función para mostrar el mensaje al comprar. 
+  fetch(CART_BUY_URL)
+  .then(obj => obj.json())
+  .then(msg => {
+    let menssege = msg.msg // guardo el mensaje en una variable. 
+    let textToImpress = document.getElementById("textFinishPurchase"); // traigo el div del html
+    textToImpress.innerHTML = `${" " + menssege}` // imprimo el mensaje de que se compro con exito
+  })
+}
+
+document.addEventListener("DOMContentLoaded",function(e){
+  getJSONData(CART_INFO_URL_DOS)
+  .then(resultObj=>{
+    if(resultObj.status === "ok"){
+      productosCarrito = resultObj.data;
+      showCartProduct();
+      
+    }
+  })
+})
